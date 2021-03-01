35a55536568b7c7a1c202d921779169238ba180c
Victor Simiao <victor.sreis@hotmail.com>
2021-03-01 16:52:57 -0300

    Tratamento de exceção - Atualizar

--- src/main/java/br/com/victorreis/eleve/service/UsuarioService.java
+++ src/main/java/br/com/victorreis/eleve/service/UsuarioService.java
@@ -3,6 +3,8 @@ package br.com.victorreis.eleve.service;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -27,26 +29,29 @@ public class UsuarioService {
 		Optional<Usuario> optional = usuarioRepository.findById(id);
 		return optional.orElseThrow(() -> new RecursoNotFoundException(id));
 	}
-	
+
 	public Usuario save(Usuario usuario) {
 		return usuarioRepository.save(usuario);
 	}
-	
-	
+
 	public void deletar(Long id) {
 		try {
-		usuarioRepository.deleteById(id);
-		}catch (EmptyResultDataAccessException e) {//Usuario que não existe
+			usuarioRepository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {// Usuario que não existe
 			throw new RecursoNotFoundException(id);
-		}catch (DataIntegrityViolationException e) { //Usuario associado
+		} catch (DataIntegrityViolationException e) { // Usuario associado
 			throw new DatabaseException(e.getMessage());
 		}
 	}
-	
+
 	public Usuario atualizar(Long id, Usuario usuarioRequest) {
-		Usuario usuarioDB = usuarioRepository.getOne(id);
-		atualizarDados(usuarioDB,usuarioRequest);
-		return usuarioRepository.save(usuarioDB);
+		try {
+			Usuario usuarioDB = usuarioRepository.getOne(id);
+			atualizarDados(usuarioDB, usuarioRequest);
+			return usuarioRepository.save(usuarioDB);
+		} catch (EntityNotFoundException e) {
+			throw new RecursoNotFoundException(id);
+		}
 	}
 
 	private void atualizarDados(Usuario usuarioDB, Usuario usuarioRequest) {
